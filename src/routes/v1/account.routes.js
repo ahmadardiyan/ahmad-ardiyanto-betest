d2e53ea4fde9601f1c337013a4b95a592363ea42
Ahmad Ardiyanto <ahmad.ardiyanto@mitrais.com>
2023-03-15 08:15:33 +0700

    feature: move route for login

--- src/routes/v1/account.routes.js
+++ src/routes/v1/account.routes.js
@@ -3,11 +3,8 @@ import AccountController from '../../controllers/account.controller.js';
 import AccountValidation from "../../validations/account.validation.js";
 
 const router = express.Router();
-const accountController = new AccountController()
+const accountController = new AccountController();
 const accountValidation = new AccountValidation();
 
-router.post('/login', 
-  accountValidation.login(),
-  async (req, res) => await accountController.login(req, res));
 
 export default router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import userRoutes from './user.routes.js'
 import accountRoutes from './account.routes.js'
+import AccountController from '../../controllers/account.controller.js';
+import AccountValidation from "../../validations/account.validation.js";
 
 const router = express.Router();
+const accountController = new AccountController()
+const accountValidation = new AccountValidation();
 
 router.route('/').get((req, res) => {
   res.send(`Hello from route v1!`);
 });
+
+router.post('/login', 
+  accountValidation.login(),
+  async (req, res) => await accountController.login(req, res));
 
 router.use('/accounts', accountRoutes)
 router.use('/users', userRoutes)
